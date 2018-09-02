40ccfac4638b32d42971277556f3e83e9e5b053a
laumlin <1255964530@qq.com>
2018-09-02 11:46:40 +0800

    微信账号和回调地址的基本配置

---
+++ config/index.js
@@ -0,0 +1,15 @@
+const config = {
+  wechat: {
+    appid: 'wechat appis',
+    appsecret: 'wechat appsecret',
+    token: 'wechat token',
+  },
+  redirect_uri: 'wechat redirect_uri',
+	mongodb: {
+		host: '127.0.0.1',
+		database: 'middleNode',
+		port: 27017
+	}
+}
+
+module.exports = config
