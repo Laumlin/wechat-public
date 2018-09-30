88a683225cc56a2448772f2fc056d61eb2fcd5c8
laumlin <1255964530@qq.com>
2018-09-30 11:16:38 +0800

    新增获取signature的路由

--- router/index.js
+++ router/index.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
 const QR = require('../controller/qr')
+const Signature = require('../controller/signature')
 router = new Router()
 
 router
-			.post('/createQr', QR.createQr)
-			.get('/getQrImage', QR.getQrImage)
-
+      .post('/createQr', QR.createQr)
+      .get('/getQrImage', QR.getQrImage)
+      .post('/getSignature', Signature.sign)
 module.exports = router;
